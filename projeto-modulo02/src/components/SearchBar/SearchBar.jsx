48668c7e5eaa5bd13e26afab632cf4f2ff130740
DevVitorPedra <75690532+DevVitorPedra@users.noreply.github.com>
2021-11-13 12:52:42 -0300

    searchbar

--- projeto-modulo02/src/components/SearchBar/SearchBar.jsx
+++ projeto-modulo02/src/components/SearchBar/SearchBar.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
 import { StyledSearch } from './styles/StyledSearchBar'
+import * as Yup from 'yup'
+import { useFormik } from 'formik'
+import {useParams} from 'react-router-dom'
 
 export default function SearchBar() {
+    const match=useParams()
+    console.log(match.url)
+    const { handleSubmit, handleChange, values, handleBlur } = useFormik({
+        initialValues: {
+            searchInput: ''
+        },
+        validationSchema: Yup.object({
+            searchInput: Yup.string()
+        })/*,
+        onSubmit: ({ searchInput }) => {
+            if (page === 'news') {
+                searchNews(searchInput)
+            } else if (page === 'gameslist') {
+                searchGames(searchInput)
+            }
+        },
+        onChange:({
+            if (page === 'news') {
+                searchNews(searchInput)
+            } else if (page === 'gameslist') {
+                searchGames(searchInput)
+            }
+            
+        })
+        */
+
+    })
     return (
-       <StyledSearch type="text"/>
+        <form onSubmit={handleSubmit} >
+            <StyledSearch onBlur={handleBlur} onChange={handleChange} value={values.searchInput} name="searchInput" placeholder="Pesquisar" type="text" />
+        </form>
+        
     )
 }
--- projeto-modulo02/src/components/SearchBar/styles/StyledSearchBar.js
+++ projeto-modulo02/src/components/SearchBar/styles/StyledSearchBar.js
@@ -5,6 +5,6 @@ export const StyledSearch = styled.input`
     border: 0px 0px 2px 0px solid black;
     transition:all 500ms ease-in-out;
     :focus{
-        background-color:gray;
+        
     }
 `
